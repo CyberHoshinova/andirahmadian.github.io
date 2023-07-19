581cd5b7495dbc2ff5653d4129019f44a22fc921
CyberHoshinova <123706725+CyberHoshinova@users.noreply.github.com>
2023-07-19 20:56:20 +0700

    Web project

--- js/script.js
+++ js/script.js
@@ -12,27 +12,27 @@ function displayBMIResult(bmi, gender) {
 
   if (gender === "male") {
     if (bmi < 20.7) {
-      message = "Anda Terlalu Kurus... Coba makan dah";
+      message = "KG Anda Terlalu Kurus... Coba makan dah karena anda Termasuk di dalam kategori Kekurangan berat badan cara terbaik untuk menaikan berat badan adalah mengkonsumsi makanan yang mengandung banyak nutrisi yang dibutuhkan.";
     } else if (bmi < 26.4) {
-      message = "Berat Anda Berada di batas Normal";
+      message = "KG Berat badan anda termasuk ideal tetap pertahankan dengan cara mengonsumsi makanan yang sehat, dan sering olahraga minimal 1 jam sehari";
     } else if (bmi < 27.8) {
-      message = "Agak berat";
+      message = "KG Agak berat berat badan anda agak berlebih ini bisa anda kurangi dengan sering olahraga dan melakukan diet atau mengonsumsi sayur dan buah";
     } else if (bmi < 31.1) {
-      message = "Terlalu Berat";
+      message = "KG Terlalu Berat. berat badan anda agak berlebih, cobalah untuk makan sayur-sayuran dan buah-buahan, lalu perbanyak aktifitas fisik seperti olahraga";
     } else {
-      message = "Obesitas sia boyyyy";
+      message = "KG Obesitas sia boyyyy";
     }
   } else {
     if (bmi < 19.1) {
-      message = "Anda Terlalu Kurus... Coba makan dah";
+      message = "KG Anda Terlalu Kurus... Coba makan dah karena anda Termasuk di dalam kategori Kekurangan berat badan cara terbaik untuk menaikan berat badan adalah mengkonsumsi makanan yang mengandung banyak nutrisi yang dibutuhkan.";
     } else if (bmi < 25.8) {
-      message = "Berat Anda Berada di batas Normal";
+      message = "KGB erat badan anda termasuk ideal tetap pertahankan dengan cara mengonsumsi makanan yang sehat, dan sering olahraga minimal 1 jam sehari";
     } else if (bmi < 27.3) {
-      message = "Agak berat";
+      message = "KG Agak berat berat badan anda agak berlebih ini bisa anda kurangi dengan sering olahraga dan melakukan diet atau mengonsumsi sayur dan buah";
     } else if (bmi < 32.3) {
-      message = "Terlalu Berat";
+      message = "KG Terlalu Berat. berat badan anda agak berlebih, cobalah untuk makan sayur-sayuran dan buah-buahan, lalu perbanyak aktifitas fisik seperti olahraga";
     } else {
-      message = "Obesitas sia boyyyy";
+      message = "KG Obesitas sia boyyyy";
     }
   }
 
